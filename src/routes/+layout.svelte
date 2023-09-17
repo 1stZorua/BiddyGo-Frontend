40768e53ba4e0b73118f71df15f6af6d651d34ca
Zorua <milandehaart@live.nl>
2023-09-17 22:43:51 +0200

    Added a shared layout file.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Header from '../components/Header.svelte';
+    import Wrapper from '../components/Wrapper.svelte';
+</script>
+
+<Wrapper>
+    <Header></Header>
+    <slot></slot>
+</Wrapper>
