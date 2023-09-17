4c669577f59a6f5ed9a256f088769ee6a4a47b00
Zorua <milandehaart@live.nl>
2023-09-17 22:42:59 +0200

    Added a Logo component.

---
+++ src/components/Logo.svelte
@@ -0,0 +1,26 @@
+<div class="logo">
+    <a href="/">
+        Biddy
+    </a>
+</div>
+
+<style lang=scss>
+    .logo {
+        font-size: $logo-font-size;
+        font-weight: $logo-font-weight;
+        
+        a {
+            color: white;
+            mix-blend-mode: difference;
+        }
+
+        &::after { 
+            content: 'Go.';
+            color: $logo-accent;
+        }
+
+        &:hover {
+            cursor: pointer;
+        }
+    }
+</style>
