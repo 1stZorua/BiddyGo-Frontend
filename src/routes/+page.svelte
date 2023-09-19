2a1a1088f3cf18b6dcb061fe72e7263ccea5ec64
Zorua <milandehaart@live.nl>
2023-09-19 23:44:19 +0200

    Updated the index.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Banner from "../components/Layout/Banner.svelte";
+	import Recommendations from "../components/Layout/Recommendations.svelte";
+</script>
+
+<Banner></Banner>
+<Recommendations></Recommendations>
