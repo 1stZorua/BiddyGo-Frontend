c002791c51ee4c2702014898138ed0cac7de869b
Zorua <milandehaart@live.nl>
2023-09-19 23:42:50 +0200

    Added a Subtitle component.

---
+++ src/components/Shared/Subtitle.svelte
@@ -0,0 +1,22 @@
+<span>
+    <slot></slot>
+</span>
+
+<style lang=scss>
+    span {
+        display: flex;
+        align-items: center;
+        gap: $line-gap;
+        font-family: $font-family-secondary;
+        font-weight: $font-weight-medium;
+        text-transform: uppercase;
+
+        &::before {
+            content: '';
+            width: $line-width;
+            background: var(--line-color);
+            height: $line-height;
+            display: inline-block;
+        }
+    }
+</style>
