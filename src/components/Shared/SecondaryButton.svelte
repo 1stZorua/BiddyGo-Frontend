c5554e515e364e4ad5358690a15fa97c1eac6278
Zorua <milandehaart@live.nl>
2023-09-19 23:42:28 +0200

    Added a Secondary Button component.

---
+++ src/components/Shared/SecondaryButton.svelte
@@ -0,0 +1,76 @@
+<script lang=ts>
+    export let text : string;
+</script>
+
+<div>
+    <button>
+        <span>
+            <slot></slot>
+        </span>
+    </button>
+    <span>{text}</span>
+</div>
+
+<style lang=scss>
+    div {
+        display: flex;
+        align-items: center;
+        gap: $btn-gap;
+
+        &:hover {
+            cursor: pointer;
+
+            button {
+                span {
+                    color: $btn-secondary;
+                }
+            
+                &::after {
+                    width: 75%;
+                    height: 75%;
+                }
+
+                &:hover {
+                    cursor: pointer;
+                }
+            }
+        }
+    }
+
+    button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: $btn-primary;
+        overflow: hidden;
+        position: relative;
+        z-index: 0;
+        background: $btn-secondary;
+        border: $btn-secondary-border;
+        border-radius: $btn-border-radius-rounded;
+        width: $icon-size;
+        height: $icon-size;
+
+        span {
+            color: $btn-primary;
+            transition: color $transition-fast;
+        }       
+
+        &::after {
+            content: '';
+            background: $btn-tertiary;
+            position: absolute;
+            width: 0;
+            height: 0;
+            z-index: -1;
+            border-radius: $btn-border-radius-rounded;
+            transition: 
+                width $transition-fast,
+                height $transition-fast;
+        }
+    }
+
+    span {
+        font-weight: $font-weight-semiBold;
+    }
+</style>
