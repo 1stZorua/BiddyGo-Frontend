74c693aa6cf2988aea85e01d840905dd73c3aef3
Zorua <milandehaart@live.nl>
2023-09-19 23:42:05 +0200

    Updated the Logo component.

---
+++ src/components/Shared/Logo.svelte
@@ -0,0 +1,25 @@
+<div class="logo">
+    <a href="/">Biddy</a>
+</div>
+
+<style lang=scss>
+    .logo {
+        font-size: $logo-font-size;
+        font-weight: $logo-font-weight;
+
+        &:after { 
+            content: 'Go.';
+            position: absolute;
+            color: $logo-accent;
+        }
+
+        &:hover {
+            cursor: pointer;
+        }
+    }
+
+    a {
+        color: white;
+        mix-blend-mode: difference;
+    }
+</style>
