e2c132d71961ddf9420446b9444cf4dae51738c3
Zorua <milandehaart@live.nl>
2023-09-19 23:41:24 +0200

    Updated the Wrapper component.

---
+++ src/components/Layout/Wrapper.svelte
@@ -0,0 +1,11 @@
+<main><slot></slot></main>
+
+<style lang=scss>
+    main {
+        display: flex;
+        flex-direction: column;
+        gap: $wrapper-gap;
+        width: $screen-width;
+        margin: $wrapper-margin;
+    }
+</style>
