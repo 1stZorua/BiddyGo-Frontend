717bc63f404c92a47f8e74d743181ddc598c224d
Zorua <milandehaart@live.nl>
2023-09-24 22:43:21 +0200

    Updated the index.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script>
     import Banner from "../components/Layout/Banner.svelte";
 	import Recommendations from "../components/Layout/Recommendations.svelte";
+    import Browse from "../components/Layout/Browse.svelte";
+    import Slogan from "../components/Layout/Slogan.svelte";
+    import MobileApp from "../components/Layout/MobileApp.svelte";
 </script>
 
 <Banner></Banner>
 <Recommendations></Recommendations>
+<Browse></Browse>
+<Slogan></Slogan>
+<MobileApp></MobileApp>
