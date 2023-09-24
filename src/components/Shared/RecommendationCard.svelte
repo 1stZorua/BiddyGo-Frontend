5b367a327a2640fe045b42667cd01267d7b49715
Zorua <milandehaart@live.nl>
2023-09-24 22:39:38 +0200

    Updated the recommendation card component.

---
+++ src/components/Shared/RecommendationCard.svelte
@@ -0,0 +1,40 @@
+<script lang=ts>
+    export let image : string;
+	import FavoriteButton from "./FavoriteButton.svelte";
+</script>
+
+<div class="card">
+    <div class="button">
+        <FavoriteButton></FavoriteButton>
+    </div>
+    <img src={image} alt="placeholder">
+</div>
+
+<style lang=scss>
+    .card {
+        position: relative;
+        overflow: hidden;
+        grid-area: var(--grid-area);
+        border-radius: $btn-border-radius-normal;
+
+        img:hover {
+            cursor: pointer;
+            scale: 1.25;
+        }
+    }
+
+    .button {
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        z-index: 1;
+    }
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        transition:
+            scale $transition-normal;
+    }
+</style>
