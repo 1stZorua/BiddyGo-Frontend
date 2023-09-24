3738e0df89374d18609a236d9a676880a4105607
Zorua <milandehaart@live.nl>
2023-09-24 22:43:11 +0200

    Updated the layout.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script>
     import Header from '../components/Layout/Header.svelte';
     import Wrapper from '../components/Layout/Wrapper.svelte';
+    import Footer from '../components/Layout/Footer.svelte';
+	import Heading from '../components/Shared/Heading.svelte';
 </script>
+
 
 <Wrapper>
     <Header></Header>
     <slot></slot>
 </Wrapper>
+<Footer></Footer>
