6668a23470a090bb8e8d693dd857dcb92e53d97f
Zorua <milandehaart@live.nl>
2023-09-26 22:45:46 +0200

    Moved file to another location.

---
+++ src/components/Shared/Objects/Logo.svelte
@@ -0,0 +1,31 @@
+<div class="logo">
+    <a href="/">
+        <div>Biddy</div>
+        <div>Go</div>
+    </a>
+</div>
+
+<style lang=scss>
+    .logo {
+        font-size: $logo-font-size;
+        font-weight: $logo-font-weight;
+
+        &:hover {
+            cursor: pointer;
+        }
+    }
+
+    a {
+        display: flex;
+        div {
+            &:first-child {
+                color: white;
+                mix-blend-mode: difference;
+            }
+    
+            &:last-child {
+                color: $logo-accent;
+            }
+        }
+    }
+</style>
