e9e11b5c872691748783db6a400b2199ab3c55c8
Zorua <milandehaart@live.nl>
2023-09-26 22:46:14 +0200

    Moved file to another location.

---
+++ src/components/Shared/Objects/SliderArrow.svelte
@@ -0,0 +1,18 @@
+<button>
+    <slot></slot>
+</button>
+
+<style lang=scss>
+    button {
+        height: $btn-height;
+        width: $btn-height;
+        background: $btn-secondary;
+        border: $btn-border-size solid $gray-regular;
+        border-radius: $btn-border-radius-rounded;
+        color: $gray-regular;
+
+        &:hover {
+            cursor: pointer;
+        }
+    }
+</style>
