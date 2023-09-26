2d980b4c81f0a7e54ffb5508bea01a4a6b10db40
Zorua <milandehaart@live.nl>
2023-09-26 22:46:50 +0200

    Updated, and changed the name of the secondary text component.

---
+++ src/components/Shared/Text/Secondary.svelte
@@ -0,0 +1,9 @@
+<span><slot></slot></span>
+
+<style lang=scss>
+    span {
+        color: var(--color);
+        font-family: $font-family-secondary;
+        text-transform: uppercase;
+    }
+</style>
