d14ab9c03216e84b4073ee067cc8827915ccc092
Zorua <milandehaart@live.nl>
2023-09-26 22:42:56 +0200

    Added a configuration for the header component.

---
+++ src/components/Layout/Header/config.js
@@ -0,0 +1,18 @@
+export const headerLinks = [
+	{
+		name: 'Home',
+		href: '#'
+	},
+	{
+		name: 'About Us',
+		href: '#'
+	},
+	{
+		name: 'Contact',
+		href: '#'
+	},
+	{
+		name: 'News',
+		href: '#'
+	}
+];
