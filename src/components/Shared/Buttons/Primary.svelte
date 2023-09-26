713855e94422c4e29e9ecd2e38299b9c5c552c7a
Zorua <milandehaart@live.nl>
2023-09-26 22:44:43 +0200

    Updated, and changed the name of the primary button component.

---
+++ src/components/Shared/Buttons/Primary.svelte
@@ -0,0 +1,49 @@
+<button>
+    <span>
+        <slot></slot>
+    </span>
+</button>
+
+<style lang=scss>
+    button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: var(--primary);
+        overflow: hidden;
+        position: relative;
+        z-index: 0;
+        background: var(--secondary);
+        border: $btn-primary-border;
+        border-radius: $btn-border-radius-rounded;
+        padding-inline: $btn-padding-inline;
+        height: $btn-height;
+
+        &::after {
+            inset: 0;
+            content: '';
+            position: absolute;
+            z-index: -2;
+            background: var(--primary);
+            width: 100%;
+            height: 100%;
+            padding: 20px;
+            transform: skew(30deg) translateX(-100%);
+            transition: 
+                transform $transition-normal;
+        }
+
+        &:hover {
+            cursor: pointer;
+
+            &::after {
+                transform: skew(30deg) translateX(0);
+            }
+        }
+    }
+
+    span {
+        color: white;
+        mix-blend-mode: difference;
+    }
+</style>
