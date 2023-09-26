fbb4ffdbbd269227e1cc3075e9fad82258a8a5d0
Zorua <milandehaart@live.nl>
2023-09-26 22:42:12 +0200

    Added a configuration file for the footer component.

---
+++ src/components/Layout/Footer/config.js
@@ -0,0 +1,39 @@
+export const navigationLinks = [
+	{
+		title: 'BiddyGo',
+		links: [
+			{ name: 'About BiddyGo', href: '#' },
+			{ name: 'Affiliate program', href: '#' },
+			{ name: 'Careers', href: '#' },
+			{ name: 'Help', href: '#' },
+			{ name: 'Press', href: '#' },
+			{ name: 'Sitemap', href: '#' }
+		]
+	},
+	{
+		title: 'Buyer Information',
+		links: [
+			{ name: 'Buyer terms', href: '#' },
+			{ name: 'Law enforcement guidelines', href: '#' },
+			{ name: 'Notarial oversight', href: '#' },
+			{ name: 'Participating in the auction', href: '#' },
+			{ name: 'Trustworthy business', href: '#' }
+		]
+	},
+	{
+		title: 'Seller Resources',
+		links: [
+			{ name: 'Photography advice', href: '#' },
+			{ name: 'Self auctioning', href: '#' },
+			{ name: 'Seller terms', href: '#' },
+			{ name: 'Submission guidelines', href: '#' }
+		]
+	},
+	{
+		title: 'My BiddyGo',
+		links: [
+			{ name: 'Login', href: '#' },
+			{ name: 'Sign up', href: '#' }
+		]
+	}
+];
