76dfcfeed7f127d13ac78013f40fd203ad9d0c2c
Zorua <milandehaart@live.nl>
2023-09-26 22:44:00 +0200

    Updated the recommendations component.

---
+++ src/components/Layout/Recommendations/Recommendations.svelte
@@ -0,0 +1,92 @@
+<script>
+    import Heading from "../../Shared/Text/Heading.svelte";
+    import TertiaryButton from "../../Shared/Buttons/Tertiary.svelte";
+    import Card from "./Card.svelte";
+    import comic from '$lib/img/comic.png';
+</script>
+
+<section class="recommendations">
+    <div class="text">
+        <Heading>Daily Recommendations</Heading>
+        <div class="separator">
+            <p>Experience daily-refreshed, personalized discoveries on BiddyGo as you log in.</p>
+            <TertiaryButton>View All</TertiaryButton>
+        </div>
+    </div>
+    <div class="content">
+        <Card image={comic} --grid-area="a"></Card>
+        <Card image={comic} --grid-area="b"></Card>
+        <Card image={comic} --grid-area="c"></Card>
+        <Card image={comic} --grid-area="d"></Card>
+        <Card image={comic} --grid-area="e"></Card>
+        <Card image={comic} --grid-area="f"></Card>
+      </div>
+</section>
+
+<style lang=scss>
+    .recommendations {
+        display: flex;
+        flex-direction: column;
+        gap: $btn-gap;
+        margin-top: $section-margin-top-medium;
+        margin-bottom: $section-margin-top-regular;
+    }
+
+    .separator {
+        position: relative;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: $btn-gap;
+    }
+
+    .content {
+        display: grid;
+        gap: 30px;
+        grid-template-areas: 
+            "a a a a b b b c c" 
+            "d d e e b b b f f";
+        height: 625px;
+    }
+
+    @media(max-width: $screen-extraLarge) {
+        .recommendations {
+            margin-top: $section-margin-top-small;
+        }
+
+        .separator {
+            flex-direction: column;
+            align-items: start;
+        }
+
+        .content {
+            grid-template-areas:
+                "a a b b" 
+                "d e b b"
+                "c e f f";
+        }
+    }
+
+    @media(max-width: $screen-large) { 
+        .content {
+            grid-template-areas:
+                "a b" 
+                "c d"
+                "e f"; 
+            height: 1100px;
+        }
+    }
+
+    @media(max-width: $screen-small) { 
+        .content {
+            grid-template-areas:
+                "a" 
+                "b"
+                "c"
+                "d"
+                "e"
+                "f"; 
+            height: 2000px;
+        }
+    }
+</style>
