baba290dc68ddb61c1c8ba4179f92a1997ad524e
Zorua <milandehaart@live.nl>
2023-09-26 22:41:23 +0200

    Updated the banner component.

---
+++ src/components/Layout/Banner/Banner.svelte
@@ -0,0 +1,99 @@
+<script>
+    import PrimaryButton from "../../Shared/Buttons/Primary.svelte";
+    import SecondaryButton from "../../Shared/Buttons/Secondary.svelte";
+	import Subtitle from "../../Shared/Text/Subtitle.svelte";
+    import Slider from "../../Layout/Banner/Slider.svelte";
+</script>
+
+<section class="banner">
+    <div class="container">
+        <div class="text">
+            <Subtitle>Auctioning quality items since 2023</Subtitle>
+            <h1>A world of auctions awaits,</h1>
+            <p>Bid confidently on our platform where every auction holds hidden treasures waiting for you to discover.</p>
+            <div class="cta">
+                <PrimaryButton --primary="white" --secondary="black">Start Bidding</PrimaryButton>
+                <SecondaryButton text={"Become a Seller"}>
+                    <i class="fa-solid fa-dollar-sign"></i>
+                </SecondaryButton>
+            </div>
+        </div>
+        <div class="display">
+            <Slider></Slider>
+        </div>
+    </div>
+</section>
+
+<style lang=scss>
+    .banner {
+        color: $primary;
+    }
+
+    .banner .container {
+        display: flex;
+        align-items: center;
+    }
+
+    .text {
+        display: flex;
+        flex-direction: column;
+        gap: $section-text-gap;
+        flex: 0.75;
+        min-width: 50%;
+
+        h1 {
+            line-height: 1.25;
+            font-size: $font-size-hero;
+
+            &::after {
+                content: ' Dive in!';
+                color: $accent-primary;
+            }
+        }
+    }
+
+    .cta {
+        margin-top: calc($btn-gap * 2);
+        display: flex;
+        gap: $btn-gap;
+    }
+
+    .display {
+        flex: 1;
+        display: flex;
+        justify-content: end;
+        position: relative;
+        height: max-content;
+        width: max-content;
+        margin-right: 100px;
+    }
+
+    @media (max-width: $screen-wide) {
+        .banner .container {
+            flex-direction: column-reverse;
+            gap: 100px;
+        }
+
+        .text {
+            align-self: start;
+        }
+
+        .display {
+            width: 100%;
+            margin-right: 0;
+        }
+    }
+
+    @media(max-width: $screen-medium) {
+        .display {
+            overflow: hidden;
+        }
+    }
+
+    @media(max-width: $screen-small) {
+        .cta {
+            margin-top: $btn-gap;
+            flex-direction: column-reverse;
+        }
+    }
+</style>
