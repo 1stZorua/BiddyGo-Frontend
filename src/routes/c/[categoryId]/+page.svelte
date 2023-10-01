b20db0779073802398c9b9fc23da2f12ccdd8783
Zorua <milandehaart@live.nl>
2023-10-01 22:47:06 +0200

    Added a categories page.

---
+++ src/routes/c/[categoryId]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { page } from "$app/stores";
+	import Categories from "../../../components/Layout/Category/Categories.svelte";
+    import Slider from "../../../components/Layout/Listing/Slider.svelte";
+    import { data } from "../../../components/Layout/Listing/data.js";
+    const categoryId = $page.params.categoryId;
+</script>
+
+<Categories></Categories>
+{#each data as category}
+    <Slider category={category}></Slider>
+{/each}
