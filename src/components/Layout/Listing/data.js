7f12d07a8cc60617f6e7859383d71b1c9fe9e0d9
Zorua <milandehaart@live.nl>
2023-10-01 22:43:34 +0200

    Created a temporary data file for auction listings.

---
+++ src/components/Layout/Listing/data.js
@@ -0,0 +1,199 @@
+import comic from '$lib/img/comic.png';
+
+export const data = [
+	{
+		title: 'Pokemon & Trading Cards',
+		cards: [
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			}
+		]
+	},
+	{
+		title: 'LEGO & Pop Culture Merchandise',
+		cards: [
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			}
+		]
+	},
+	{
+		title: 'Video Games & Computers',
+		cards: [
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			},
+			{
+				image: comic,
+				title: 'The Pokemon Company Mystery box - Blastoise',
+				current_bid: 70,
+				time_remaining: '7 days'
+			}
+		]
+	}
+];
