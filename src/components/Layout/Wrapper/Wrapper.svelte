79a4f8a2e782bbc19a2abb81c6fa68ae585e2e21
Zorua <milandehaart@live.nl>
2023-10-01 22:45:08 +0200

    Updated the wrapper component.

--- src/components/Layout/Wrapper/Wrapper.svelte
+++ src/components/Layout/Wrapper/Wrapper.svelte
@@ -7,12 +7,13 @@
         gap: $wrapper-gap;
         margin: $wrapper-margin-normal;
         max-width: $screen-width;
+        width: calc(100% - $wrapper-margin-inline-normal * 2);
         height: max-content;
     }
 
     @media (max-width: $screen-medium) {
         main {
-            margin: $wrapper-margin-small;
+            width: calc(100% - $wrapper-margin-inline-small * 2);
         }
     }
 </style>
