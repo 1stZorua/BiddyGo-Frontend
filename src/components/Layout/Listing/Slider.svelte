5cca35df6f5b8826bbad43db8fcb53e50e379068
Zorua <milandehaart@live.nl>
2023-10-24 19:54:35 +0200

    Updated slider component.

--- src/components/Layout/Listing/Slider.svelte
+++ src/components/Layout/Listing/Slider.svelte
@@ -1,87 +1,59 @@
 <script lang=ts>
-	import Card from "../Listing/Card.svelte";
-    import Subheading from "../../Shared/Text/Subheading.svelte";
-	import TertiaryButton from "../../Shared/Buttons/Tertiary.svelte";
-    
-    /** @type {{
-        title: string,
-        cards: Array<object>
-    }} */
-    export let category : any;
+	import { onMount } from "svelte";
+	import { sendRequest } from "$lib/functions/request.ts";
+	import type { AuctionListing, SubCategory } from "$lib/types/types";
+    import { Subheading, TertiaryButton, Slider } from "../../index.ts";
+	import Card from "./Card.svelte";
 
-    let slider: HTMLElement;
-    let isDown: boolean = false;
-    let startX: number;
-    let scrollLeft: number;
-    let animationFrameId: number;
+    let auctionListings : Array<AuctionListing> = []
+    let loaded: boolean = false;
 
-    function onSliderMouseDown(e: MouseEvent) {
-        isDown = true;
-        startX = e.pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
+    export let active: boolean = true;
+    export let subCategory : SubCategory = {
+        id: 0,
+        name: "Pokemon & Trading Cards",
+        imageId: 0,
+        categoryId: 0,
+    };
 
-        cancelAnimationFrame(animationFrameId);
-    }
-
-    function onSliderMouseUpOrLeave() {
-        if (isDown) flickEffect();
-        isDown = false;
-    }
-
-    function onSliderMouseMove(e: MouseEvent) {
-        if (!isDown) return;
-        e.preventDefault();
-        const x = e.pageX - slider.offsetLeft;
-        const walk = (x - startX) * 2;
-        slider.scrollLeft = scrollLeft - walk;
-    }
-
-    function flickEffect() {
-        let flickVelocity = slider.scrollLeft - scrollLeft;
-        const maxVelocity = 50;
-
-        function animateScroll() {
-            flickVelocity *= 0.95;
-
-            if (Math.abs(flickVelocity) > maxVelocity) {
-                flickVelocity = flickVelocity > 0 ? maxVelocity : -maxVelocity;
-            }
-
-            slider.scrollLeft += flickVelocity;
-
-            if (Math.abs(flickVelocity) < 0.1) {
-                cancelAnimationFrame(animationFrameId);
-                scrollLeft = slider.scrollLeft;
-            } else {
-                animationFrameId = requestAnimationFrame(animateScroll);
-            }
-        }
-
-        animateScroll();
-    }
+    onMount(async() => {
+        if (!active) return;
+        auctionListings = await sendRequest(`/api/AuctionListing/subcategories/${subCategory.id}`, "GET");
+        loaded = true;
+    });
 </script>
 
-<section class="slider">
-    <div class="separator">
-        <Subheading>{category.title}</Subheading>
-        <TertiaryButton>View All</TertiaryButton>
-    </div>
-    <div class="container" class:is-active={isDown}
-        role="presentation" 
-        aria-label="Image Carousel"
-        bind:this={slider}
-        on:mousedown={onSliderMouseDown}
-        on:mouseleave={onSliderMouseUpOrLeave}
-        on:mouseup={onSliderMouseUpOrLeave}
-        on:mousemove={onSliderMouseMove}
-    >
-        {#each category.cards as cardData}
-            <div>
-                <Card cardData={cardData}></Card>
+{#if loaded} 
+    {#if auctionListings.length > 0}
+        <section class="slider" id={subCategory.name}>
+            <div class="separator">
+                <Subheading>{subCategory.name}</Subheading>
+                <TertiaryButton>View All</TertiaryButton>
             </div>
-        {/each}
-    </div>
-</section>
+            <Slider --position="absolute" --margin-top="120px">
+                {#each auctionListings as auctionListing}
+                    <div>
+                        <Card auctionListing={auctionListing}></Card>
+                    </div>
+                {/each}
+            </Slider>
+        </section>
+    {/if}
+{:else}
+    <section class="slider">
+        <div class="separator">
+            <Subheading active={loaded}>{subCategory.name}s</Subheading>
+            <TertiaryButton active={loaded}>View All</TertiaryButton>
+        </div>
+        <Slider --position="absolute" --margin-top="120px">
+            {#each Array(10) as _}
+                <div>
+                    <Card active={loaded}></Card>
+                </div>
+            {/each}
+        </Slider>
+    </section>
+{/if}
 
 <style lang=scss> 
     .slider {
@@ -94,29 +66,6 @@
     .separator {
         display: flex;
         justify-content: space-between;
-    }
-
-    .container {
-        margin-top: 120px;
-        position: absolute;
-        display: flex;
-        gap: 50px;
-        user-select: none;
-        width: 100%;
-        overflow-x: scroll;
-        padding-right: 100px;
-
-        -ms-overflow-style:none;
-        scrollbar-width: none;
-
-        &::-webkit-scrollbar {
-            display: none;
-        }
-    }
-
-    .container.is-active {
-        cursor: grabbing;
-        cursor: -webkit-grabbing;
     }
 
     @media(max-width: $screen-extraLarge) { 
