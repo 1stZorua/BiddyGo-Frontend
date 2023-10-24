1a8fce8fe207b97d08ebb6c09e2b0e33896e4f6b
Zorua <milandehaart@live.nl>
2023-10-24 19:57:25 +0200

    Updated subheading component.

--- src/components/Shared/Text/Subheading.svelte
+++ src/components/Shared/Text/Subheading.svelte
@@ -1,9 +1,21 @@
-<h3>
+<script lang=ts>
+    export let active: boolean = true;
+</script>
+
+<h3 data-active={active}>
     <slot></slot>
 </h3>
 
 <style lang=scss>
     h3 {
         font-size: $font-size-medium;
+        font-weight: var(--font-weight, $font-weight-bold);
+    }
+
+    [data-active="false"] {
+        background: $skeleton-background-color;
+        background-size: 200% 100%; 
+        animation: $skeleton-animation;
+        color: transparent;
     }
 </style>
