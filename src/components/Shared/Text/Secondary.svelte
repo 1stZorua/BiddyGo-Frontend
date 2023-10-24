fdc10f037e03ea5d5b38055a6947b9f2211aef41
Zorua <milandehaart@live.nl>
2023-10-24 19:57:10 +0200

    Updated secondary component.

--- src/components/Shared/Text/Secondary.svelte
+++ src/components/Shared/Text/Secondary.svelte
@@ -1,9 +1,25 @@
-<span><slot></slot></span>
+<script lang=ts>
+    export let active: boolean = true;
+</script>
+
+<span data-active={active}>
+    <slot></slot>
+</span>
 
 <style lang=scss>
     span {
         color: var(--color);
-        font-family: $font-family-secondary;
+        font-family: var(--font-family, $font-family-secondary);
+        font-weight: var(--font-weight, $font-weight-regular);
         text-transform: var(--text-transform, uppercase);
     }
+
+    [data-active="false"] {
+        background: $skeleton-background-color;
+        background-size: 200% 100%; 
+        animation: $skeleton-animation;
+        color: transparent;
+        width: max-content;
+    }
 </style>
+
