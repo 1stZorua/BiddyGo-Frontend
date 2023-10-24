b9f2349c7e791ca3fc9c9d9ad845e968667cdbd0
Zorua <milandehaart@live.nl>
2023-10-24 19:56:55 +0200

    Added medium component.

---
+++ src/components/Shared/Text/Medium.svelte
@@ -0,0 +1,23 @@
+<script lang=ts>
+    export let active: boolean = true;
+</script>
+
+<span data-active={active}>
+    <slot></slot>
+</span>
+
+<style lang=scss>
+    span {
+        color: var(--color, $primary);
+        font-size: $font-size-medium;
+        font-weight: var(--font-weight, $font-weight-medium);
+        width: max-content;
+    }
+
+    [data-active="false"] {
+        background: $skeleton-background-color;
+        background-size: 200% 100%; 
+        animation: $skeleton-animation;
+        color: transparent;
+    }
+</style>
