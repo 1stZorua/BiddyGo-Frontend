4fb24d7c80b48494c6d30583067c101bd29b3139
Zorua <milandehaart@live.nl>
2023-10-24 19:56:32 +0200

    Updated slider arrow component.

--- src/components/Shared/Objects/SliderArrow.svelte
+++ src/components/Shared/Objects/SliderArrow.svelte
@@ -10,9 +10,14 @@
         border: $btn-border-size solid $gray-regular;
         border-radius: $btn-border-radius-rounded;
         color: $gray-regular;
+        transition: 
+            color $transition-fast,
+            border-color $transition-fast;
 
         &:hover {
             cursor: pointer;
+            color: $gray-dark;
+            border-color: $gray-dark;
         }
     }
 </style>
