d50cfab2240389e7125836524bfd599c662cef74
Zorua <milandehaart@live.nl>
2023-10-24 20:01:33 +0200

    Updated category page.

--- src/routes/c/[categoryId]/+page.svelte
+++ src/routes/c/[categoryId]/+page.svelte
@@ -1,12 +1,29 @@
-<script>
+<script lang=ts>
     import { page } from "$app/stores";
-	import Categories from "../../../components/Layout/Category/Categories.svelte";
-    import Slider from "../../../components/Layout/Listing/Slider.svelte";
-    import { data } from "../../../components/Layout/Listing/data.js";
-    const categoryId = $page.params.categoryId;
+	import { onMount } from "svelte";
+	import { sendRequest } from "$lib/functions/request";
+	import type { SubCategory } from "$lib/types/types";
+    import { Categories, ListingSlider } from "../../../components/index.ts";
+
+    const categoryId: string = $page.params.categoryId;
+
+    let subCategories: Array<SubCategory> = [];
+    let loaded: boolean = false;
+    
+    onMount(async() => {
+        subCategories = await sendRequest(`/api/Category/subcategories/${categoryId}`, "GET");
+        loaded = true;
+    });
+    
 </script>
 
-<Categories></Categories>
-{#each data as category}
-    <Slider category={category}></Slider>
-{/each}
+<Categories categoryId={categoryId}></Categories>
+{#if loaded}
+    {#each subCategories as subCategory}
+        <ListingSlider subCategory={subCategory}></ListingSlider>
+    {/each}
+{:else}
+    {#each Array(3) as _}
+        <ListingSlider active={loaded}></ListingSlider>
+    {/each}
+{/if}
