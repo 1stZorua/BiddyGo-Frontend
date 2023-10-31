18ecaa4ac056e4a0c411fde9545d31b3c381bbe9
Zorua <milandehaart@live.nl>
2023-10-31 22:25:10 +0100

    Updated view auction listing page file.

--- src/routes/c/[categoryId]/[subCategoryId]/[auctionListingId]/+page.svelte
+++ src/routes/c/[categoryId]/[subCategoryId]/[auctionListingId]/+page.svelte
@@ -1,7 +1,5 @@
 <script lang=ts>
     import { page } from "$app/stores";
-	import { onMount } from "svelte";
-	import { sendRequest } from "$lib/functions/request";
     import { ViewListing } from "../../../../../components/index.ts";
 
     const auctionListingId: string = $page.params.auctionListingId;
