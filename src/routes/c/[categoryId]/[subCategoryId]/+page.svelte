43229b1aab7d2f0030de09c4efdd8f1126d7a27f
Zorua <milandehaart@live.nl>
2023-11-05 22:30:47 +0100

    Updated page.

--- src/routes/c/[categoryId]/[subCategoryId]/+page.svelte
+++ src/routes/c/[categoryId]/[subCategoryId]/+page.svelte
@@ -1,10 +1,7 @@
 <script lang=ts>
     import { page } from "$app/stores";
-	import { onMount } from "svelte";
-	import { sendRequest } from "$lib/functions/request";
 
     const subCategoryId: string = $page.params.subCategoryId;
-
 </script>
 
 <div>{subCategoryId}</div>
