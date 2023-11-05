3e3ca7f027a29ba1f952080675078e9ef87100f3
Zorua <milandehaart@live.nl>
2023-11-05 22:30:53 +0100

    Updated page.

--- src/routes/c/[categoryId]/[subCategoryId]/[auctionListingId]/+page.svelte
+++ src/routes/c/[categoryId]/[subCategoryId]/[auctionListingId]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang=ts>
     import { page } from "$app/stores";
-    import { ViewListing } from "../../../../../components/index.ts";
+    import { ViewListing, PageModal } from "../../../../../components/index.ts";
 
     const auctionListingId: string = $page.params.auctionListingId;
 </script>
