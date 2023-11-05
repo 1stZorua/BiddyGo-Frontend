df3548c35e8b27577583be1063c9db701c8e2412
Zorua <milandehaart@live.nl>
2023-11-05 22:30:22 +0100

    Updated the layout page file.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
+    import { onMount } from "svelte";
+    import { scrollLock } from "../stores/scrollLockStore.ts";
     import { Header, Wrapper, Footer } from "../components/index.ts";
+  
+    onMount(() => {
+        scrollLock.subscribe(
+            () => document.body.setAttribute("data-scrollLock", $scrollLock ? "true" : "false")
+        );
+    });
 </script>
 
 <Wrapper>
