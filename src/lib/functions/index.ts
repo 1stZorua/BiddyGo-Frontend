bae81db178fb09a583137a5cb6384810ab6823de
Zorua <milandehaart@live.nl>
2023-11-05 22:26:52 +0100

    Added barrel file for functions.

---
+++ src/lib/functions/index.ts
@@ -0,0 +1,13 @@
+// api
+export * from './api/fetchMultipleRequests.ts';
+export * from './api/request.ts';
+export * from './api/signalR.ts';
+
+//bidding
+export * from './bidding/calculateBidIncrements.ts';
+export * from './bidding/placeBid.ts';
+
+//utility
+export * from './utility/formatCurrency.ts';
+export * from './utility/scroll.ts';
+export * from './utility/truncateText.ts';
