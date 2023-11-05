ab299e4c5de97be0d60d28534c4661bbee37155f
Zorua <milandehaart@live.nl>
2023-11-05 22:26:26 +0100

    Added tablist component.

---
+++ src/components/Shared/Objects/Tablist.svelte
@@ -0,0 +1,66 @@
+<script lang=ts>
+    import { onMount } from "svelte";
+    import { FilterButton } from "../../index.ts";
+
+    let activeButton: number = 0;
+    let tablistContent: HTMLDivElement;
+    let content: Array<HTMLDivElement>;
+
+    function onTabClick(index: number) {
+        if (index === activeButton) return;
+
+        activeButton = index;
+        content.forEach((c, i) => {
+            if (i === index) {
+                c.classList.remove("hidden");
+                return;
+            }
+            c.classList.add("hidden");
+        });
+    }
+
+    export let active: boolean = true;
+    export let sections: string[] = [];
+
+    onMount(() => {
+        content = Array.from(tablistContent.querySelectorAll(".content > div"));
+    });
+</script>
+
+<div class="information">
+    <div class="tablist">
+        {#each sections as tab, index}
+            <button on:click={() => onTabClick(index)}>
+                <FilterButton active={active} activeButton={activeButton} number={index} onClick={onTabClick}>
+                    {tab}
+                </FilterButton>
+            </button>
+        {/each}
+    </div>
+    <div 
+        class="content"
+        bind:this={tablistContent} 
+    >
+        <slot></slot>
+    </div>
+</div>
+
+<style lang=scss>
+    .information {
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+    }
+
+    .tablist {
+        display: flex;
+        flex-wrap: wrap;
+        gap: $btn-gap;
+    }
+
+    .tablist button {
+        flex: 1;
+        border: none;
+        background: none;
+    }
+</style>
