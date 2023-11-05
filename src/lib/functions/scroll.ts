db2ddd183cc6965f21b12e7f6cedf06e0b9da733
Zorua <milandehaart@live.nl>
2023-11-05 22:27:07 +0100

    Moved file to new location.

--- src/lib/functions/scroll.ts
+++
@@ -1,4 +0,0 @@
-export default function scrollTo(element: string, id: boolean = false) {
-    if (id) { document.getElementById(element)?.scrollIntoView({ block: "start", behavior: "smooth" }); return; }
-    document.querySelector(element)?.scrollIntoView({ behavior: "smooth" });
-}
