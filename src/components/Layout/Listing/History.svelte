e26a66c63984def7fbe48cbfb850d7679935b31b
Zorua <milandehaart@live.nl>
2023-11-07 19:44:41 +0100

    Updated bid history component.

--- src/components/Layout/Listing/History.svelte
+++ src/components/Layout/Listing/History.svelte
@@ -3,7 +3,7 @@
     import { backOut } from "svelte/easing";
     import type { Bid } from "$lib/types/types.ts";
     import { formatCurrency } from "$lib/functions";
-    import { MediumText, Subheading } from "../../index.ts";
+    import { MediumText, SmallText, Subheading } from "../../index.ts";
     import flag_netherlands from "$lib/img/flag_netherlands.svg";
 
     let seeAllBids: boolean = false;
@@ -24,22 +24,20 @@
                     <td>
                         <div class="bidder">
                             <img src={flag_netherlands} alt="origin">
-                            <MediumText --font-weight={index === 0 ? "600" : "400"}>Bidder {bid.bidder_id}</MediumText>
+                            <SmallText --font-weight={index === 0 ? "600" : "400"}>Bidder {bid.bidder_id}</SmallText>
                         </div>
                     </td>
-                    <div>
-                        <td>
-                            <MediumText --font-weight={index === 0 ? "600" : "400"}>{index + 1} h ago</MediumText>
-                        </td>
-                        <td>
-                            <MediumText --font-weight={index === 0 ? "600" : "400"}>&euro;{formatCurrency(bid.amount)}</MediumText>
-                        </td>
-                    </div>
+                    <td>
+                        <SmallText --font-weight={index === 0 ? "600" : "400"}>{index + 1} h ago</SmallText>
+                    </td>
+                    <td>
+                        <SmallText --font-weight={index === 0 ? "600" : "400"}>&euro;{formatCurrency(bid.amount)}</SmallText>
+                    </td>
                 </tr>
             {/each}
         </tbody>
         <button on:click={onSeeAllBidsClick}>
-            <MediumText>See all bids ({bids.length})</MediumText>
+            <MediumText>See all bids ({bids.length > 3 ? bids.length : 0})</MediumText>
             <i class="fa-solid fa-chevron-down"></i>
         </button>
         {#if seeAllBids}
@@ -47,22 +45,20 @@
                 class="other__bids" 
                 transition:fly={{ y: -50, easing: backOut }}      
             >
-                {#each [...bids].reverse().slice(3, bids.length) as bid}
+                {#each [...bids].reverse().slice(3, bids.length) as bid, index}
                     <tr>
                         <td>
                             <div class="bidder">
                                 <img src={flag_netherlands} alt="origin">
-                                <MediumText --font-weight="400">Bidder {bid.bidder_id}</MediumText>
+                                <SmallText --font-weight="400">Bidder {bid.bidder_id}{index}</SmallText>
                             </div>
                         </td>
-                        <div>
-                            <td>
-                                <MediumText --font-weight="400">4 h ago</MediumText>
-                            </td>
-                            <td>
-                                <MediumText --font-weight="400">&euro;{formatCurrency(bid.amount)}</MediumText>
-                            </td>
-                        </div>
+                        <td>
+                            <SmallText --font-weight="400">41 h ago</SmallText>
+                        </td>
+                        <td>
+                            <SmallText --font-weight="400">&euro;{formatCurrency(bid.amount)}</SmallText>
+                        </td>
                     </tr>
                 {/each}
             </tbody>
@@ -80,7 +76,7 @@
     .bidder {
         display: flex;
         align-items: center;
-        gap: 10px;
+        gap: 5px;
 
         img {
             width: 30px;
@@ -89,12 +85,32 @@
         }
     }
 
-    tr > div {
+    tbody {
         display: flex;
-        justify-content: space-between;
+        flex-direction: column;
+        gap: 10px;
+    }
+
+    tr {
+        display: flex;
+        align-items: center;
         
-        >:first-child {
-            margin-right: 100px;
+        td {
+            vertical-align: middle;
+            white-space: nowrap;
+
+            &:first-child {
+                width: 35%;
+            }
+
+            &:nth-child(2) {
+                text-align: left;
+                width: 35%;
+            }
+
+            &:last-child {
+                margin-left: auto;
+            }
         }
     }
 
@@ -111,6 +127,12 @@
         &:hover {
             cursor: pointer;
             opacity: 0.5;
+        }
+    }
+
+    @media (max-width: $screen-wide) {
+        tr td:first-child {
+            width: 50%;
         }
     }
 </style>
