9c95f3bac6ecf839175de1982c238e05adccc5ef
Zorua <milandehaart@live.nl>
2023-11-12 22:55:18 +0100

    Added the login page.

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,9 @@
+<script lang=ts>
+    import { LoginForm, RegisterForm } from "../../components/index.ts";
+	import type { PageData } from "./$types";
+
+    export let data: PageData
+</script>
+
+<LoginForm data={data.loginForm}></LoginForm>
+<RegisterForm data={data.registerForm}></RegisterForm>
