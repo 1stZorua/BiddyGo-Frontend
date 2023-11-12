58146573b1b2f6c4685fe1e42d1d4bbb08517aa4
Zorua <milandehaart@live.nl>
2023-11-12 22:48:18 +0100

    Updated the app interfaces.

--- src/app.d.ts
+++ src/app.d.ts
@@ -7,6 +7,12 @@ declare global {
 		// interface Locals {}
 		// interface PageData {}
 		// interface Platform {}
+		interface PageData {
+			flash?: {
+				type: "success" | "error";
+				message: string;
+			}
+		}
 	}
 }
 
