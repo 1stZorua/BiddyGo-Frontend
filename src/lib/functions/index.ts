d179f7da0ac672837bb31c8d2bdd6e60bc8dd2a5
Zorua <milandehaart@live.nl>
2023-11-12 22:52:00 +0100

    Updated the functions barrel file.

--- src/lib/functions/index.ts
+++ src/lib/functions/index.ts
@@ -1,10 +1,13 @@
 // api
+export const API_URL = 'https://localhost:32774';
+
 export * from './api/fetchMultipleRequests.ts';
 export * from './api/request.ts';
 export * from './api/signalR.ts';
 
 //bidding
 export * from './bidding/calculateBidIncrements.ts';
+export * from './bidding/remainingTime.ts';
 export * from './bidding/placeBid.ts';
 
 //utility
