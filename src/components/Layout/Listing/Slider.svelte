080042063262dfcb9f3c3c09fa5eebbf802d1265
Zorua <milandehaart@live.nl>
2023-11-14 22:44:19 +0100

    Updated the auction listing slider component.

--- src/components/Layout/Listing/Slider.svelte
+++ src/components/Layout/Listing/Slider.svelte
@@ -1,25 +1,15 @@
 <script lang=ts>
-	import { onMount } from "svelte";
-    import { sendRequest } from "$lib/functions/index.ts";
 	import type { AuctionListing, SubCategory } from "$lib/types/types";
 	import { defaultSubCategory } from "$lib/types/defaults.ts";
     import { Subheading, TertiaryButton, Slider } from "../../index.ts";
 	import Card from "./Card.svelte";
 
     export let active: boolean = true;
-    export let subCategory : SubCategory = defaultSubCategory;
-
-    let auctionListings : Array<AuctionListing> = []
-    let loaded: boolean = false;
-
-    onMount(async() => {
-        if (!active) return;
-        auctionListings = await sendRequest(`/api/AuctionListing/subcategories/${subCategory.id}`, "GET");
-        loaded = true;
-    });
+    export let subCategory: SubCategory = defaultSubCategory;
+    export let auctionListings: Array<AuctionListing> = [];
 </script>
 
-{#if loaded} 
+{#if active}
     {#if auctionListings.length > 0}
         <section class="slider" id={subCategory.name}>
             <div class="separator">
@@ -38,13 +28,13 @@
 {:else}
     <section class="slider">
         <div class="separator">
-            <Subheading active={loaded}>{subCategory.name}s</Subheading>
-            <TertiaryButton active={loaded}>View All</TertiaryButton>
+            <Subheading active={active}>{subCategory.name}s</Subheading>
+            <TertiaryButton active={active}>View All</TertiaryButton>
         </div>
         <Slider --position="absolute" --margin-top="120px">
             {#each Array(10) as _}
                 <div>
-                    <Card active={loaded}></Card>
+                    <Card active={active}></Card>
                 </div>
             {/each}
         </Slider>
