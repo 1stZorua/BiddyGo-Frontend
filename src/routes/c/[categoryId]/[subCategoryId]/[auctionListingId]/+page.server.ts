dbadc8c852b17fe89132af6814ba96bb0f9f5b88
Zorua <milandehaart@live.nl>
2023-11-14 22:46:52 +0100

    Updated the server load file for the view auction listing page.

--- src/routes/c/[categoryId]/[subCategoryId]/[auctionListingId]/+page.server.ts
+++ src/routes/c/[categoryId]/[subCategoryId]/[auctionListingId]/+page.server.ts
@@ -1,11 +1,26 @@
 import { fail } from "@sveltejs/kit";
 import { superValidate } from "sveltekit-superforms/server";
 import { bidSchema } from "$lib/schemas/bid";
+import { fetchMultipleRequests } from "$lib/functions/index.js";
+import type { PageServerLoad } from "./$types.js";
 
-export const load = async (event) => {
-    const form = await superValidate(event, bidSchema);
+export const load: PageServerLoad = async (event) => {
+    const auctionListingId = event.params.auctionListingId;
+    const formData = await superValidate(event, bidSchema);
+    const [auctionListing, auctionListingImages, currentBid, bidHistory] = await fetchMultipleRequests(
+        [
+            { url: `/api/AuctionListing/${auctionListingId}`, method: "GET" },
+            { url: `/api/AuctionListing/images/${auctionListingId}`, method: "GET" },
+            { url: `/api/Bid/highest/${auctionListingId}`, method: "GET" },
+            { url: `/api/Bid/${auctionListingId}`, method: "GET" }
+        ]
+    );
     return { 
-        form 
+        formData,
+        auctionListing,
+        auctionListingImages, 
+        currentBid,
+        bidHistory
     };
 }
 
