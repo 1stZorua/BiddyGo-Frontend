a85fb24ae32963892af426eb3c449ab391ce3d51
Zorua <milandehaart@live.nl>
2023-11-14 22:47:13 +0100

    Updated the view auction listing page.

--- src/routes/c/[categoryId]/[subCategoryId]/[auctionListingId]/+page.svelte
+++ src/routes/c/[categoryId]/[subCategoryId]/[auctionListingId]/+page.svelte
@@ -1,11 +1,47 @@
 <script lang=ts>
-    import { ViewListing } from "../../../../../components/index.ts";
-	import { setContext } from "svelte";
-	import type { PageData } from "./$types";
+    import { page } from "$app/stores";
+	import { afterNavigate } from "$app/navigation";
+	import { onDestroy, onMount } from "svelte";
+    import { ViewListing, PageModal } from "../../../../../components/index.ts";
+	import { connectToHub, disconnectFromHub, invokeHubMethod, isPageModalOpen } from "../../../../../stores/index.ts";
+	import { calculateRemainingTime, useSignalRHub } from "$lib/functions/index.ts";
+    import type { Bid, ViewData } from "$lib/types/types.ts";
+    import { API_URL } from "$lib/functions/index.ts";
 
-    export let data: PageData
+    export let data: ViewData;
 
-    $: setContext("bidData", data.form);
+    let loaded: boolean = false;
+    let interval: NodeJS.Timeout;
+
+    $: if (data.auctionListing) {
+        data.remainingTime = calculateRemainingTime(new Date(data.auctionListing.endTime.toString()).getTime()) 
+        if (interval == null) {
+            interval = setInterval(() => { 
+                data.remainingTime = calculateRemainingTime(new Date(data.auctionListing.endTime.toString()).getTime()) 
+            }, 1000);
+        }
+    }
+
+    afterNavigate(() => loaded = true);
+    
+    onMount(async() => {
+        connectToHub(await useSignalRHub<Bid>(
+            `${API_URL}/auction-hub`, "NewBid", 
+            (res) => { 
+                data.currentBid = res; 
+                data.bidHistory = [...data.bidHistory, data.currentBid]; 
+            }
+        ));
+        invokeHubMethod("JoinGroup", `auction_${$page.params.auctionListingId}`);
+    });
+
+    onDestroy(() => {
+        clearInterval(interval);
+        disconnectFromHub();
+    });
 </script>
 
-<ViewListing></ViewListing>
+<ViewListing {loaded} {data}></ViewListing>
+{#if $isPageModalOpen}
+    <PageModal {...data} thumbnailImage={data.auctionListingImages[0]}></PageModal>
+{/if}
