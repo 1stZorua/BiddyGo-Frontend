a359ef64fdea4a7ed76aac21b22ce9d121cdd0f1
Zorua <milandehaart@live.nl>
2023-11-14 22:46:08 +0100

    Updated the layout server load file.

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,6 @@
 import type { LayoutServerLoad } from "./$types";
 import { loadFlash } from "sveltekit-flash-message/server";
 
-export const load: LayoutServerLoad = loadFlash(async (event) => {
-    return {
-        someData: "Flash messages enabled"
-    };
+export const load: LayoutServerLoad = loadFlash(async () => {
+ 
 });
