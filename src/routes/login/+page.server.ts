54e5aeada3e03ee6456bc4bd0d02722e118f2e0d
Zorua <milandehaart@live.nl>
2023-11-14 22:47:59 +0100

    Updated the server load file of the login page.

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -2,9 +2,9 @@ import { fail } from "@sveltejs/kit";
 import { superValidate } from "sveltekit-superforms/server";
 import { loginSchema } from "$lib/schemas/login.js";
 import { registerSchema } from "$lib/schemas/register.js";
-import type { User } from "$lib/types/types.js";
 import { sendRequest } from "$lib/functions/index.js";
 import { redirect } from "sveltekit-flash-message/server";
+import type { User } from "$lib/types/types.js";
 
 export const load = async (event) => {
     const loginForm = await superValidate(event, loginSchema);
@@ -22,9 +22,31 @@ export const actions = {
 
         if (!form.valid) return fail(400, { form });
 
+        let token: string;
+
+        try {
+            const user: User = {
+                email: form.data.email,
+                password: form.data.password,
+            }
+
+            token = await sendRequest<string>(`/api/User/login`, "POST", user);
+        } catch (error) {
+            console.error((error as Error).message);
+            if ((error as Error).message.includes("Not Found")) {
+                return fail(400, { error: "Email or password is incorrect", form });
+            }
+            return fail(500, { error: "Internal server error", form });
+        }
+
+        console.log(token);
+
         throw redirect(
-            "/", 
-            { type: "success", message: "Succesfully Logged in."}, 
+            "/",
+            { 
+                type: "success", 
+                message: "Succesfully logged in."
+            }, 
             event
         );
     },
@@ -35,10 +57,32 @@ export const actions = {
 
         if (!form.valid) return fail(400, { form });
 
-        const user: User = await sendRequest(`/api/User/${form.data.email}`, "GET");
+        try {
+            const user = await sendRequest(`/api/User/email/${form.data.email}`, "GET");
+            
+            if (user) {
+                return fail(400, { error: "User already exists", form });
+            }
+        } catch (error) {
+            if ((error as Error).message.includes("Not Found")) {
+                const newUser = {
+                    email: form.data.email,
+                    password: form.data.password,
+                };
 
-        if (user) {
-            return fail(400, { form });
+                await sendRequest(`/api/User`, "POST", newUser);
+            } else {
+                return fail(500, { error: "Internal server error", form });
+            }
         }
+
+        throw redirect(
+            "/login",
+            {
+                type: "success",
+                message: "Successfully created account.",
+            },
+            event,
+        );
     }
 }
