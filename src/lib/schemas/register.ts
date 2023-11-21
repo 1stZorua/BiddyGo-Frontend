0bc45715d80fa9548b6fad0c1e7a7c216513649d
Zorua <milandehaart@live.nl>
2023-11-21 22:57:53 +0100

    Updated register schema.

--- src/lib/schemas/register.ts
+++ src/lib/schemas/register.ts
@@ -4,6 +4,9 @@ export const registerSchema = z.object({
     email: z.string().email(),
     password: z.string().min(5),
     confirmPassword: z.string().min(5)
-})
+}).refine(data => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"]
+});
 
 export type RegisterSchema = typeof registerSchema;
