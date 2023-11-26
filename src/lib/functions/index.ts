619c6800104d5648da0721d03aa282c6b3f37433
Zorua <milandehaart@live.nl>
2023-11-26 22:57:05 +0100

    Updated the functions barrel file.

--- src/lib/functions/index.ts
+++ src/lib/functions/index.ts
@@ -1,16 +1,20 @@
-// api
+// Api
 export const API_URL = 'https://localhost:32770';
 
 export * from './api/fetchMultipleRequests.ts';
 export * from './api/request.ts';
 export * from './api/signalR.ts';
 
-//bidding
+// Bidding
 export * from './bidding/calculateBidIncrements.ts';
-export * from './bidding/remainingTime.ts';
+export * from './bidding/getStatus.ts';
+export * from './listing/remainingTime.ts';
 export * from './bidding/placeBid.ts';
 
-//utility
+// Listing
+export * from './listing/addFavorite.ts';
+
+// Utility
 export * from './utility/formatCurrency.ts';
 export * from './utility/scroll.ts';
 export * from './utility/truncateText.ts';
