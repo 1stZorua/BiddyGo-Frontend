89652f43f1052d69ea0ba229609f5f6900877dfc
Zorua <milandehaart@live.nl>
2023-11-26 23:04:29 +0100

    Updated the server side rendering of the sell page.

--- src/routes/sell/+page.server.ts
+++ src/routes/sell/+page.server.ts
@@ -1,38 +1,71 @@
 import { fail } from "@sveltejs/kit";
 import { listingSchema } from "$lib/schemas/listing.ts";
 import { superValidate } from "sveltekit-superforms/server";
+import { fetchMultipleRequests, sendRequest } from "$lib/functions/index.js";
+import type { AuctionListing } from "$lib/types/types.js";
 import { redirect } from "sveltekit-flash-message/server";
+import { API_URL } from "$lib/functions/index.js";
 
 export const load = async (event) => {
     if (!event.locals.user) {
         throw redirect(302, "/login");
     }
 
+    const [categories, subCategories] = await fetchMultipleRequests(
+        [
+            { url: "/api/Category", method: "GET" },
+            { url: "/api/SubCategory", method: "GET" }
+        ]
+    );
+
     const form = await superValidate(event, listingSchema);
     return { 
-        form
+        form,
+        categories,
+        subCategories
     };
 }
 
 export const actions = {
     create: async (event) => {
         const clonedRequest = event.request.clone();
-
         const form = await superValidate(event, listingSchema);
-        console.log(form);
-    
         const formData = await clonedRequest.formData();
+        const images = formData.getAll("image");
 
-        const images = formData.getAll("images");
-        const test = formData.getAll("image");
+        const test = new FormData();
 
-        console.log(test);
-        console.log(test.length);
+        images.forEach((image) => {
+            test.append("image", image);
+        });
 
-        console.log("---");
-        console.log(images);
-        console.log(images.length);
 
         if (!form.valid) return fail(400, { form });
+
+        const auctionListing: AuctionListing = {
+            title: form.data.title,
+            description: form.data.description,
+            subCategoryId: form.data.subCategoryId,
+            sellerId: Number(event.locals.user.id),
+            startTime: new Date(Date.now()),
+            endTime: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            startingBid: 0,
+        }
+
+        const result = await sendRequest<AuctionListing, AuctionListing>("/api/AuctionListing", "POST", auctionListing);
+        await fetch(`${API_URL}/api/AuctionListing/images/${result.id}`, {
+            method: 'POST',
+            body: formData,
+        });
+
+        throw redirect(
+            302,
+            "/login",
+            {
+                type: "success",
+                message: "Successfully created auction listing.",
+            },
+            event,
+        );
     }
 }
