cbf0afea6d1cc96c2dfe3a5238591446aef844a6
Zorua <milandehaart@live.nl>
2023-11-26 23:04:06 +0100

    Updated the view auction listing page.

--- src/routes/c/[categoryId]/[subCategoryId]/[auctionListingId]/+page.svelte
+++ src/routes/c/[categoryId]/[subCategoryId]/[auctionListingId]/+page.svelte
@@ -4,8 +4,8 @@
 	import { onDestroy, onMount } from "svelte";
     import { ViewListing, PageModal } from "../../../../../components/index.ts";
 	import { connectToHub, disconnectFromHub, invokeHubMethod, isPageModalOpen } from "../../../../../stores/index.ts";
-	import { calculateRemainingTime, useSignalRHub } from "$lib/functions/index.ts";
-    import type { Bid, ViewData } from "$lib/types/types.ts";
+	import { calculateRemainingTime, getStatus, useSignalRHub } from "$lib/functions/index.ts";
+    import type { Bid, CallbackMap, ViewData } from "$lib/types/types.ts";
     import { API_URL } from "$lib/functions/index.ts";
 
     export let data: ViewData;
@@ -18,19 +18,33 @@
         if (interval == null) {
             interval = setInterval(() => { 
                 data.remainingTime = calculateRemainingTime(new Date(data.auctionListing.endTime.toString()).getTime()) 
-            }, 1000);
+            }, 500);
         }
     }
 
+    $: data.bidText = getStatus(data.currentBid, data.remainingTime, $page.data.user);
+
     afterNavigate(() => loaded = true);
+
+    const callbacks: CallbackMap = {
+        "NewBid": (res: Bid) => { 
+            data.currentBid = res; 
+            data.bidHistory = [...data.bidHistory, data.currentBid];
+            data.currentBid.time = new Date();
+        },
+        "AddFavorite": (id: number, remove: boolean) => {
+            if (remove) {
+                data.favorites.splice(data.favorites.indexOf(id), 1);
+                return;
+            }
+            data.favorites = [...data.favorites, id];
+        }
+    };
     
     onMount(async() => {
-        connectToHub(await useSignalRHub<Bid>(
-            `${API_URL}/auction-hub`, "NewBid", 
-            (res) => { 
-                data.currentBid = res; 
-                data.bidHistory = [...data.bidHistory, data.currentBid]; 
-            }
+        connectToHub(await useSignalRHub(
+            `${API_URL}/auction-hub`,
+            callbacks
         ));
         invokeHubMethod("JoinGroup", `auction_${$page.params.auctionListingId}`);
     });
