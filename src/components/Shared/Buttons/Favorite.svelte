5d73f1cf7185a3c22f535f5bb4a083465dd23a43
Zorua <milandehaart@live.nl>
2023-11-26 22:55:00 +0100

    Updated the favorite button component.

--- src/components/Shared/Buttons/Favorite.svelte
+++ src/components/Shared/Buttons/Favorite.svelte
@@ -1,15 +1,29 @@
-<button>   
-    <i class="fa-regular fa-heart"></i>
+<script lang=ts>
+    export let active: boolean = true;
+    export let isFavorited: boolean = false;
+    export let onClick: () => void = () => {};
+</script>
+
+<button data-active={active} on:click={onClick}>   
+    <i 
+        class="fa-solid fa-heart"
+        class:fa-regular={!isFavorited}
+        class:fa-solid={isFavorited}
+    ></i>
+    <slot></slot>
 </button>
 
 <style lang=scss>
     button {
-        display: flex;
-        justify-content: center;
-        align-items: center;
+        display: grid;
+        grid-auto-flow: column;
+        justify-items: center;
+        place-items: center;
+        gap: 5px;
         background: $btn-secondary;
         color: $btn-primary;
-        width: $btn-height;
+        width: max-content;
+        padding-inline: 20px;
         height: $btn-height;
         border-radius: $btn-border-radius-rounded;
         outline: $btn-border-size solid transparent;
@@ -26,5 +40,9 @@
                 font-weight: 800;
             }
         }
+    }
+
+    button[data-active="false"] {
+        display: none;
     }
 </style>
