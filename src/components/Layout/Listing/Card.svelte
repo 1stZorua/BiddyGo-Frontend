a25b6c137282f44eda5d2f2ef1efd1531bff5d07
Zorua <milandehaart@live.nl>
2023-11-26 22:54:28 +0100

    Updated the auction listing card component.

--- src/components/Layout/Listing/Card.svelte
+++ src/components/Layout/Listing/Card.svelte
@@ -1,32 +1,96 @@
 <script lang=ts>
+    import { page } from "$app/stores";
     import { onMount } from "svelte";
-    import { calculateRemainingTime, fetchMultipleRequests, formatCurrency, formatRemainingTime, truncateText } from "$lib/functions/index.ts";
-    import type { AuctionListing, Bid, Image, RemainingTime } from "$lib/types/types";
+    import { addFavorite, calculateRemainingTime, fetchMultipleRequests, formatCurrency, formatRemainingTime, getStatus, sendRequest, truncateText } from "$lib/functions/index.ts";
+    import type { AuctionListing, Bid, Favorite, Image, RemainingTime, SubCategory } from "$lib/types/types";
 	import { defaultAuctionListing, defaultRemainingTime } from "$lib/types/defaults.ts";
-    import { SecondaryText, SmallText, MediumText } from "../../index.ts";
+    import { SecondaryText, SmallText, MediumText, FavoriteButton } from "../../index.ts";
+    import { getFlash } from "sveltekit-flash-message/client";
+    import { hubConnection } from "../../../stores/hubStore.ts";
+
+    const flash = getFlash(page);
 
     export let active: boolean = true;
     export let auctionListing: AuctionListing = defaultAuctionListing;
+    export let format: "grid" | "lines" = "grid";
 
+    let loaded: boolean = false;
+    let subCategory: SubCategory;
     let thumbnailImage: Image;
     let currentBid: Bid;
     let remainingTime: RemainingTime = defaultRemainingTime;
+    let previousAuctionListing: AuctionListing;
+    let favorites: number[] = [];
+    
+    $: isFavorited = favorites.includes(Number($page.data.user?.id)) ? true : false
+    $: bidText = getStatus(currentBid, remainingTime, $page.data.user);
+    $: {
+        if (auctionListing !== previousAuctionListing) {
+            fetchData();
+            previousAuctionListing = auctionListing;
+        }
+    }
 
-    onMount(async() => {
+    async function fetchData() {
         if (!active) return;
-        const [bidResponse, thumbnailResponse] = await fetchMultipleRequests(
+        const [subCategoryResponse, bidResponse, thumbnailResponse, favoritesResponse] = await fetchMultipleRequests(
             [
+                { url: `/api/SubCategory/${auctionListing.subCategoryId}`, method: "GET" },
                 { url: `/api/Bid/highest/${auctionListing.id}`, method: "GET" },
-                { url: `/api/AuctionListing/thumbnail/${auctionListing.id}`, method: "GET" }
+                { url: `/api/AuctionListing/thumbnail/${auctionListing.id}`, method: "GET" },
+                { url: `/api/Favorite/auction-listing/${auctionListing.id}`, method: "GET" }
             ]
         );
-        [currentBid, thumbnailImage] = [bidResponse as Bid, thumbnailResponse as Image];
+        [subCategory, currentBid, thumbnailImage, favorites] = [subCategoryResponse as SubCategory, bidResponse as Bid, thumbnailResponse as Image, favoritesResponse as number[]];
 
         remainingTime = calculateRemainingTime(new Date(auctionListing.endTime.toString()).getTime())
-    });
+        loaded = true;
+    }
+
+    async function onFavorite() {
+        if (!$page.data.user) {
+            flash.set({type: "error", message: "You need to be logged in to favorite an auction listing."});
+            return;
+        }
+
+        const favorite: Favorite = {
+            auctionListingId: auctionListing.id,
+            userId: Number($page.data.user.id)
+        }
+
+        if (favorites.includes(favorite.userId)) {
+            favorites.splice(favorites.indexOf(favorite.userId), 1);
+        } else {
+            favorites.push(favorite.userId);
+        }
+
+        isFavorited = favorites.includes(favorite.userId);
+        favorites.length = favorites.length;
+
+        await sendRequest(`/api/Favorite`, "POST", favorite);
+        await addFavorite($hubConnection, favorite.userId, favorite.auctionListingId);
+    }
+
+    onMount(async() => fetchData());
 </script>
 
-<a rel="external" href={active ? [window.location.href, auctionListing.subCategoryId, auctionListing.id].join("/") : ""} class="card" draggable="false" data-sveltekit-preload-data>
+<a 
+    rel="external" 
+    href={loaded ? ["/c", subCategory.categoryId, subCategory.id, auctionListing.id].join("/") : ""} 
+    class="card" 
+    class:lines={format === "lines"}
+    draggable="false" 
+    data-sveltekit-preload-data
+>
+    <button class="favorite" on:click|preventDefault>
+        <FavoriteButton 
+            active={loaded}
+            isFavorited={isFavorited}
+            onClick={onFavorite}
+        >
+            <SmallText --height="max-content">{favorites.length}</SmallText>
+        </FavoriteButton>
+    </button>
     <div class="display">
         {#if thumbnailImage}
             <img src={"data:image/jpeg;base64," + thumbnailImage?.fileContents} alt={auctionListing.title} draggable="false" loading="lazy">
@@ -35,21 +99,22 @@
         {/if}
     </div>
     <div class="text">
-        <SmallText active={active}>{truncateText(auctionListing.title, 50)}</SmallText>
+        <SmallText --height="60px" active={loaded}>{truncateText(auctionListing.title, 50)}</SmallText>
         <div>
-            <SecondaryText active={active} --color="#7A7A7A">Current Bid</SecondaryText>
+            <SecondaryText active={loaded} --color={bidText.includes("You") ? "#57C5B6" : "#7A7A7A"}>{bidText}</SecondaryText>
             <MediumText active={currentBid ? true : false}>&euro; {currentBid ? formatCurrency(currentBid.amount) : 0}</MediumText>
         </div>
-        <SecondaryText active={active} --color="#7A7A7A" --font-family="Poppins" --font-weight="500" --text-transform="normal">{formatRemainingTime(remainingTime, "other")} left</SecondaryText>
+        <SecondaryText active={loaded} --color="#7A7A7A" --font-family="Poppins" --font-weight="500" --text-transform="normal">{formatRemainingTime(remainingTime, "other")} left</SecondaryText>
     </div>
 </a>
 
 <style lang=scss>
 .card {
+    position: relative;
     display: flex;
     flex-direction: column;
     gap: 10px;
-    width: 300px;
+    width: var(--width, 300px);
     overflow: hidden;
     border-radius: $btn-border-radius-normal $btn-border-radius-normal 0 0;
 
@@ -60,11 +125,44 @@
     }
 }
 
+.card.lines {
+    flex-direction: row;
+    gap: 30px;
+    justify-content: center;
+    border-radius: $btn-border-radius-normal 0 0 $btn-border-radius-normal;
+
+    .display {
+        flex: 1;
+        width: 350px;
+        
+        img {
+            border-radius: $btn-border-radius-normal 0 0 $btn-border-radius-normal;
+        }
+    }
+
+    .text { 
+        flex: 1;
+        justify-content: space-between;
+
+        div {
+            margin-top: auto;
+        }
+    }
+}
+
+.favorite {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    z-index: 10;
+}
+
 .display {
-    height: 350px;
     overflow: hidden;
+    height: 350px;
 
     img {
+        object-fit: cover;
         border-radius: $btn-border-radius-normal $btn-border-radius-normal 0 0;
         width: 100%;
         height: 100%;
@@ -91,6 +189,28 @@
     div {
         display: flex;
         flex-direction: column;
+    }
+}
+
+@media(max-width: $screen-medium) {
+    .card.lines {
+        flex-direction: column;
+        justify-content: normal;
+        gap: 4px;
+        border-radius: $btn-border-radius-normal $btn-border-radius-normal 0 0;
+
+        .display {
+            width: 100%;
+
+            img {
+                height: 350px;
+                border-radius: $btn-border-radius-normal $btn-border-radius-normal 0 0;
+            }
+        }
+
+        .text { 
+            flex: 0;
+        }
     }
 }
 </style>
