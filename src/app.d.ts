43c5d4813b38e0095bc322b2aced31f8057f6a04
Zorua <milandehaart@live.nl>
2023-11-26 22:52:32 +0100

    Updated the global locals interface.

--- src/app.d.ts
+++ src/app.d.ts
@@ -12,12 +12,18 @@ declare global {
 				type: "success" | "error";
 				message: string;
 			}
+			user?: {
+				id: string,
+				email: string,
+				role: string
+			}
 		}
 
 		interface Locals {
 			user: { 
+				id: string,
 				email: string,
-				role: string,
+				role: string
 			}
 		}
 	}
