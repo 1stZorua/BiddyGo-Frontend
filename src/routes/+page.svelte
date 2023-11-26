22dcd6bad326efcf6fc5b7a670ab17c78ad64657
Zorua <milandehaart@live.nl>
2023-11-26 22:59:52 +0100

    Updated the index page file.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang=ts>
+	import type { IndexData } from "$lib/types/types.ts";
     import { Banner, Recommendations, Browse, Slogan, MobileApp } from "../components/index.ts";
+
+    export let data: IndexData;
 </script>
 
-<Banner></Banner>
+<Banner auctionListing={data.latestAuctionListing.data[0]}></Banner>
 <Recommendations></Recommendations>
-<Browse></Browse>
+<Browse categories={data.categories.data}></Browse>
 <Slogan></Slogan>
 <MobileApp></MobileApp>
