8feb957ecd51900aa32df2bd387437f4eb66b5ba
2ndZorua <485892@student.fontys.nl>
2023-12-01 20:56:25 +0100

    Updated the get status function test.

--- src/tests/getStatus.test.ts
+++ src/tests/getStatus.test.ts
@@ -1,95 +1,44 @@
 import { getStatus } from '$lib/functions'; 
-import type { Bid, LocalUser } from '$lib/types/types';
+import type { Bid, LocalUser, RemainingTime } from '$lib/types/types';
 import { it, expect } from 'vitest';
 
-it('returns "You\'ve won!" when the user is the highest bidder and the auction has ended', () => {
-    //Arrange
-    const currentBid : Bid = {
+const createTestSetup = (days: number, hours: number, minutes: number, seconds: number, userId: string): { currentBid: Bid, remainingTime: RemainingTime, user: LocalUser } => {
+    const currentBid: Bid = {
         id: "0",
         auction_listing_id: 0,
         bidder_id: 0,
         amount: 10,
         time: new Date(Date.now())
-    }; 
-    const remainingTime = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+    };
+    const remainingTime: RemainingTime = { days: days, hours: hours, minutes: minutes, seconds: seconds };
     const user: LocalUser = {
-        id: "0",
+        id: userId,
         email: "test@gmail.com",
         role: "user"
     };
+    return { currentBid, remainingTime, user };
+};
 
-    //Act
+it('returns "You\'ve won!" when the user is the highest bidder and the auction has ended', () => {
+    const { currentBid, remainingTime, user } = createTestSetup(0, 0, 0, 0, "0");
     const result = getStatus(currentBid, remainingTime, user);
-
-    //Assert
     expect(result).toBe("You've won!");
 });
 
 it('returns "You\'re winning" when the user is the highest bidder and the auction is still active', () => {
-    //Arrange
-    const currentBid : Bid = {
-        id: "0",
-        auction_listing_id: 0,
-        bidder_id: 0,
-        amount: 10,
-        time: new Date(Date.now())
-    }; 
-    const remainingTime = { days: 1, hours: 2, minutes: 30, seconds: 15 };
-    const user: LocalUser = {
-        id: "0",
-        email: "test@gmail.com",
-        role: "user"
-    };
-
-    //Act
+    const { currentBid, remainingTime, user } = createTestSetup(1, 2, 30, 15, "0");
     const result = getStatus(currentBid, remainingTime, user);
-
-    //Assert
     expect(result).toBe("You're winning");
 });
 
 it('returns "Ended" when the user is not the highest bidder and the auction has ended', () => {
-    //Arrange
-    const currentBid : Bid = {
-        id: "0",
-        auction_listing_id: 0,
-        bidder_id: 0,
-        amount: 10,
-        time: new Date(Date.now())
-    };
-    const remainingTime = { days: 0, hours: 0, minutes: 0, seconds: 0 };
-    const user: LocalUser = {
-        id: "1",
-        email: "test@gmail.com",
-        role: "user"
-    };
-
-    //Act
+    const { currentBid, remainingTime, user } = createTestSetup(0, 0, 0, 0, "1");
     const result = getStatus(currentBid, remainingTime, user);
-
-    //Assert
     expect(result).toBe("Ended");
 });
 
 it('returns "Current Bid" when the user is not the highest bidder and the auction is still active', () => {
-    //Arrange
-    const currentBid : Bid = {
-        id: "0",
-        auction_listing_id: 0,
-        bidder_id: 0,
-        amount: 10,
-        time: new Date(Date.now())
-    }; 
-    const remainingTime = { days: 1, hours: 2, minutes: 30, seconds: 15 };
-    const user: LocalUser = {
-        id: "1",
-        email: "test@gmail.com",
-        role: "user"
-    };
-
-    //Act
+    const { currentBid, remainingTime, user } = createTestSetup(1, 2, 30, 15, "1");
     const result = getStatus(currentBid, remainingTime, user);
-
-    //Assert
     expect(result).toBe("Current Bid");
 });
