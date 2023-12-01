cb1cd998703d55e9ce12194578cd57e76dd76bc1
2ndZorua <485892@student.fontys.nl>
2023-12-01 20:35:37 +0100

    Updated the calculate bid increments function.

--- src/lib/functions/bidding/calculateBidIncrements.ts
+++ src/lib/functions/bidding/calculateBidIncrements.ts
@@ -1,11 +1,15 @@
 export function calculateBidIncrements(currentBid: number) {
-    const increments = currentBid < 500
-        ? [10, 20, 30]
-        : currentBid < 1000
-        ? [20, 40, 60]
-        : currentBid < 1500
-        ? [50, 100, 150]
-        : [100, 200, 300];
-        
+    let increments;
+
+    if (currentBid < 500) {
+        increments = [10, 20, 30];
+    } else if (currentBid < 1000) {
+        increments = [20, 40, 60];
+    } else if (currentBid < 1500) {
+        increments = [50, 100, 150];
+    } else {
+        increments = [100, 200, 300];
+    }
+
     return increments.map(increment => currentBid + increment);
 }
