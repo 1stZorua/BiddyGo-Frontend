ff5db8baa2927958aa27a05ac75f2c47266a2a3f
2ndZorua <485892@student.fontys.nl>
2023-12-01 19:28:07 +0100

    Updated functions barrel file.

--- src/lib/functions/index.ts
+++ src/lib/functions/index.ts
@@ -1,5 +1,5 @@
 // Api
-export const API_URL = 'https://localhost:32777';
+export const API_URL = 'https://localhost:32772';
 
 export * from './api/fetchMultipleRequests.ts';
 export * from './api/request.ts';
